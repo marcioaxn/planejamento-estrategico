db2bc014de52d8a8792ec39631d339a9c08d7437
Marcio Alessandro Xavier Neto <marcio.xavierneto@gmail.com>
2024-09-21 13:50:53 -0300

    20240921-13h50 - Correção e ajustes na página do mapa estratégico para finalizar a agregação correta dos indicadores ligados aos objetivos estratégicos.

--- resources_20240921/views/vendor/jetstream/components/button-danger.blade.php
+++
@@ -1,3 +0,0 @@
-<button-danger {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-red-300 disabled:opacity-25 transition']) }} style="cursor:pointer;">
-    {{ $slot }}
-</button-danger>
--- resources_20240921/views/vendor/jetstream/components/labelopcional.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}  <span class="text-blue-300" style="font-size: 0.7rem!Important;">(opcional )</span>
-</label>
